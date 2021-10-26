78ddc5a74565db42e597dea9200096f715df3039
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2021-10-26 15:51:21 +0200

    OK

---
+++ src/test/java/com/cfa/univeristy/UniveristyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfa.univeristy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniveristyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
